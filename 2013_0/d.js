e2f12eba9902c9d11cfac2928e6e5bad38683e19
Elliot Foster <efoster@firetaco.com>
2013-04-13 21:26:03 -0700

    2013 Qualifying problem D small progress
    My depth-first search is terrible.

--- 2013_0/d.js
+++ 2013_0/d.js
@@ -7,45 +7,105 @@
     cat input.file | ./d.js  > output.file
 */
 
+function numify(s) { return parseInt(s, 10); }
+
 var fs    = require('fs')
   , print = require('prettyjson').render
   , _     = require('underscore')
   , str   = require('underscore.string')
-  , input = fs.readFileSync('/dev/stdin').toString().split(/\n/g)
+  , input = fs.readFileSync('/dev/stdin').toString().split(/\n/g).map(function(l){
+    return l.split(' ').map(numify);
+  })
   , cnum  = 0
 ;
 
 var cases = input.shift();
 
-function numify(s) { return parseInt(s, 10); }
+function clone(data) {
+  return JSON.parse(JSON.stringify(data));
+}
+
+function iterate(keys, opened, remain) {
+  var attempts = [];
+
+  //console.log(keys, opened, remain);
+  //console.log(opened);
+
+  if (opened.length == remain.length) {
+    return {
+      keys: keys
+      , opened: opened
+      , remain: remain
+    };
+  }
+
+  var k, o, r, need, have;
+  //for (var i = 0, l = remain.length; i < l; ++i) {
+  remain.forEach(function(item, i){
+    var k = clone(keys);
+    var o = clone(opened);
+    var r = clone(remain);
+    if (!r[i]) return;
+
+    need = r[i].req;
+    have = k.indexOf(need);
+
+    if (have === -1) return;
+
+    //console.log(o, i, need, have, k);
+
+    k.splice(have, 1); // remove key once used
+
+    k.push(r[i].keys); // add new keys, if any
+    k = _.flatten(k);
+    r[i] = null;
+
+    o.push(i);
+
+    attempts.push(iterate(k,o,r));
+  });
+  //}
+
+  var success = _.find(attempts, function(a){
+    if (a && a.opened.length === a.remain.length) {
+      return a.opened;
+    }
+    return false;
+  });
+
+  if (success) {
+    return success;
+  }
+}
 
 while (cnum++ < cases) {
-  var l = input.shift().split(' ').map(numify);
+  var l = input.shift();
   var numKeys  = l[0]
     , numChest = l[1]
   ;
 
-  var keys = input.shift().split(' ').map(numify);
+  var keys = input.shift();
   var chests = [];
 
   _.range(numChest).forEach(function(i){
-    var l = input.shift().split(' ').map(numify);
+    var l = input.shift();
     var req  = l.shift();
     var nk = l.shift();
     var chest = {
-      r: req
-      , k: l
+      req: req
+      , keys: l
     };
 
+    //console.log(chest);
     chests.push(chest);
   });
 
-  console.warn(print({
-    k:   keys
-    , c: chests
-  }));
+  var res = iterate(keys, [], chests);
 
-  var res;
+  var text = 'IMPOSSIBLE';
+  if (res) {
+    text = res.opened.map(function(a){return ++a;}).join(' ');
+  }
 
-  console.log(str.sprintf('Case #%d: %s', cnum, res));
+  console.log(str.sprintf('Case #%d: %s', cnum, text));
 }
