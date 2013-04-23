526ba682be18b46438fbff86a5e0f1bb955eadcf
Elliot Foster <efoster@firetaco.com>
2013-04-23 10:47:36 -0700

    2012 Qualifying problem A cleaned up

--- 2012_0/a.js
+++ 2012_0/a.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 
-// http://code.google.com/codejam/contest/1460488/dashboard#s=p0
+/*
 
-var gcj   = require('./codejam');
-var input = gcj.getInput('skip_parsing');
-var cnum  = 0;
+  http://code.google.com/codejam/contest/1460488/dashboard#s=p0
+
+  develop with (running in the current directory):
+    ../node_modules/.bin/chicken -c "clear; cat A-sample.in | ./a.js" .
+
+*/
+
+var fs    = require('fs')
+  , _     = require('underscore')
+  , str   = require('underscore.string')
+  , input = fs.readFileSync('/dev/stdin').toString().split(/\n/g)
+  , cnum  = 0
+  , cases = input.shift()
+;
 
 var coded = " zqejpmysljylckdkxveddknmcrejsicpdrysirbcpcypcrtcsradkhwyfrepkymveddknkmkrkcddekrkdeoyakwaejtysrreujdrlkgcjv";
 var clear = " qzourlanguageisimpossibletounderstandtherearetwentysixfactorialpossibilitiessoitisokayifyouwanttojustgiveup";
@@ -14,15 +25,11 @@ for (var i = 0, l = coded.length; i < l; ++i) {
   lookup[coded[i]] = clear[i];
 }
 
-while (++cnum <= gcj.numCases) {
-    var str = input.shift().join(' ');
-    var r = [];
-    for (var i = 0, l = str.length; i < l; ++i) {
-        r.push(lookup[str[i]]);
+while (++cnum <= cases) {
+    var trans = input.shift().split('');
+    for (var i = 0, l = trans.length; i < l; ++i) {
+      trans[i] = lookup[trans[i]];
     }
 
-    console.log("Case #{c}: {r}".supplant({
-          'c': cnum
-        , 'r': r.join('')
-    }));
+    console.log(str.sprintf("Case #%d: %s", cnum, trans.join('')));
 }
