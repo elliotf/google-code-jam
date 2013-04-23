628fa4951e94e86b97c7a346c13ed2b25ea1d5ea
Elliot Foster <efoster@firetaco.com>
2013-04-23 11:19:38 -0700

    2012 Qualifying problem C cleaned up

--- 2012_0/c.js
+++ 2012_0/c.js
@@ -1,44 +1,50 @@
 #!/usr/bin/env node
 
-// http://code.google.com/codejam/contest/1460488/dashboard#s=p2
+/*
 
-var gcj   = require('./codejam');
-var input = gcj.getInput();
-var numc  = gcj.numCases;
-var cnum  = 0;
+  http://code.google.com/codejam/contest/1460488/dashboard#s=p2
 
-while (++cnum <= numc) {
-    var line = input.shift();
-    var a = line.shift();
-    var b = line.shift();
+  develop with (running in the current directory):
+    ../node_modules/.bin/chicken -c "clear; cat C-sample.in | ./c.js" .
 
-    var r = 0;
-    var seen = {};
+*/
 
-    for (var i = a; i <= b; ++i) {
-        var c = i.toString();
-        if (seen[c]) continue;
-        seen[c] = true;
+var fs  = require('fs')
+  , _   = require('underscore')
+  , str   = require('underscore.string')
+  , input = fs.readFileSync('/dev/stdin').toString().split(/\n/g)
+  , cnum  = 0
+  , cases = input.shift()
+;
 
-        var h = c + c;
-        var p = 0;
-        var l = c.length;
+while (++cnum <= cases) {
+  var line = input.shift().split(' ').map(function(n){return parseInt(n, 10);});
+  var a = line.shift();
+  var b = line.shift();
 
-        for (var j = l; j > 1; --j) {
-            var n = c.substring(j-1) + c.substring(0,j-1);
-            if (n[0] === '0' || n === c || seen[n]) continue;
-            seen[n] = true;
+  var r = 0;
+  var seen = {};
 
-            if (n < a || n > b) continue;
-            if (h.indexOf(n) > 0) {
-                ++p;
-                r = r + p;
-            }
-        }
+  for (var i = a; i <= b; ++i) {
+    var c = i.toString();
+    if (seen[c]) continue;
+    seen[c] = true;
+
+    var p = 0;
+    var l = c.length;
+
+    // brute force, moving end of stringified number to beginning
+    // node.js is fast enough: solving the large set takes ~1min
+    for (var j = l; j > 1; --j) {
+      var n = c.substring(j-1) + c.substring(0,j-1);
+      if (n[0] === '0' || n === c || seen[n]) continue;
+      seen[n] = true;
+
+      if (n < a || n > b) continue;
+      ++p;
+      r = r + p;
     }
+  }
 
-    console.log("Case #{cnum}: {r}".supplant({
-          'cnum': cnum
-          , 'r': r
-    }));
+  console.log(str.sprintf("Case #%d: %s", cnum, r));
 }
