403cfdbd42fd5294723f56b02849cc3cc140e2f9
Elliot Foster <efoster@firetaco.com>
2014-04-13 22:47:11 -0700

    2012 Qualifying problem C rewrite WIP
    So...  many..  corner cases...

--- 2014_0/c.js
+++ 2014_0/c.js
@@ -17,125 +17,159 @@ function res(cnum, result) {
   console.log(str.sprintf('Case #%d:\n%s', cnum, result));
 }
 
-function board_string(x,y,mines,non_mines) {
-  var impossible = false;
-  var non_x = Math.ceil(Math.sqrt(non_mines));
-  var non_y = Math.floor(non_mines/non_x);
-
-  if (non_y > y) {
-    non_y = y;
-
-    non_x = Math.floor(non_mines / y);
-  }
-
-  if (non_x > x) {
-    non_x = x;
-
-    non_y = Math.floor(non_mines / x);
-  }
-
-  var non_remainder = (non_mines - non_x*non_y);
-
-  console.warn("mines", mines);
-  console.warn("non_x, non_y", non_x, non_y);
-  console.warn("non_remainder", non_remainder);
-
-  if ((x > 1 && y > 1) && (x <= 3 || y <= 3)) {
-    if (non_remainder) {
-      impossible = true;
-    }
-  }
-
-  // if x or y are less than 3 we need to be careful
-  // otherwise, just start printing stuff
-
-  // bounds check against x and y
-
-  //console.log("x,    y,    m        ", x,    y,    mines);
-  //console.log("non_x,non_y,non_mines", non_x,non_y,non_mines);
-
-  // basically, a click needs to be at least dist 2 from every mine
-  // I think all we need:
-  //   * four non-mine spaces if we have a two-dimensional minefield
-  //   * two non-mine spaces if we have a one-dimensional minefield
-
-  // drawing the minefield
-  //   * the click is always top-left
-  //   * get width/height of non-mine area
-  //     * x = Math.ciel(sqrt(non_mines)); y = Math.floor(non_mines/x);
-
-  // if we have multiple non-mine lines equal to two
-  // and they differ
-  // try to make them equal
-
-  var num_lines = Math.ceil(non_mines / x);
-  console.warn("num_lines", num_lines);
-  console.warn("non_mines, x", non_mines, x);
-
-  if (num_lines < 3) {
-    // ensure lines are equal-ish?
-  }
-
-  var iy = 0, ix = 0;
-  var nc = non_mines - 1;
-  var mc = mines;
-  var ny = non_y;
-  if (non_remainder) {
-    ny++;
-  }
-
-  var b = [];
-  var row;
-  var nx = non_x;
-  for(iy = 0; iy < y; iy++) {
-    var row = [];
-
-    if (ny-- > 0) {
-      nx = non_x;
-    }
-
-    if (0 === iy) {
-      row.push('c');
-      ix = 1;
-      nx--;
-    } else {
-      ix = 0;
-    }
-
-    for(ix; ix < x; ++ix) {
-      if (nx-- > 0 && nc) {
-        nc--;
-        row.push('.');
-      } else {
-        row.push('*');
-      }
-    }
-
-    b.push(row.join(''));
-  }
-
-  if (impossible) {
-    console.warn(b.join('\n'));
-    return 'Impossible';
-  }
-
-  return b.join('\n');
-}
+// when non-mines outnumber mines
+//   make a square of mines in the bottom-right
+// when mines outnumber non-mines
+//   make a square of non-mines in the top-left
 
 _.range(1,num_cases + 1).map(function(cnum){
   console.warn("");
 
-  var tmp   = input.shift().split(' ').map(function(n){ return Number(n); });
-  var y     = tmp.shift();
-  var x     = tmp.shift();
-  var mines = tmp.shift();
+  var tmp      = input.shift().split(' ').map(function(n){ return Number(n); });
+  var y        = tmp.shift();
+  var x        = tmp.shift();
+  var mines    = tmp.shift();
+  var non      = x*y - mines;
+  var rotate   = false;
+  var fail     = 'Impossible';
+  var last_non = [];
 
-  var non_mines = x*y - mines;
-
-  var min_non = (x > 1 && y > 1) ? 4 : 2;
-  if (non_mines < min_non) {
-    return res(cnum, 'Impossible');
+  if (x > y) {
+    console.warn("flipping");
+    rotate = true;
+    tmp = x;
+    x = y;
+    y = tmp;
   }
 
-  res(cnum, board_string(x,y,mines,non_mines));
+  var non_rows      = Math.floor(non / x);
+  var non_remainder = Math.floor(non % x);
+
+  function flip(b) {
+    var y = b.length;
+    var t;
+
+    for (var i = 0; i < b.length; ++i) {
+      for (var j = 0; j < i; ++j) {
+        t = b[i][j];
+        b[i][j] = b[j][i];
+        b[j][i] = t;
+      }
+    }
+  }
+
+  function str(b) {
+    if (rotate) {
+      flip(b);
+    }
+
+    return b.map(function(r){return r.join('')}).join('\n');
+  }
+
+  function fill(b) {
+    var n = non;
+
+    b.forEach(function(row, y){
+      row.forEach(function(field, x){
+        if (n-- > 0) {
+          row[x] = '.';
+        }
+        if (n === 0) {
+          last_non = [y,x];
+        }
+      });
+    });
+  }
+
+  function solve() {
+    // check for min non-mines
+    // either one or two (for 1d) or one or four (for 2d)
+
+    if (x > 2 && (non === 2 || non === 3)) {
+      return fail;
+    }
+
+    if (x == 2) {
+      // can't have an odd number of mines if we're only two wide
+      if (mines % 2) return fail;
+    }
+
+    var b = [];
+    _.range(1,y+1).forEach(function(){
+      var r = [];
+      _.range(1,x+1).forEach(function() {
+        r.push('*');
+      });
+
+      b.push(r);
+    });
+
+    fill(b);
+
+    console.warn("non_rows, non", non_rows, non);
+    if (non_rows === 0 && non > 1) {
+      console.warn("should fail");
+      return fail;
+    }
+
+    if (non_rows === 1 && !non_remainder && non > 1) {
+      if (non % 2) {
+        //return fail
+      }
+    }
+
+    if (non_rows === 1 && non > 1 && non % 2) {
+      return fail;
+    }
+
+    if (non_rows === 1) {
+      // make both lines mines and make a block of non-mines
+      b[0] = _.range(x).map(function(){return '*';});
+      b[1] = _.range(x).map(function(){return '*';});
+
+      for (var r = 0; r < 2; ++r) {
+        for (var c = 0; c < non / 2; ++c) {
+          b[r][c] = '.';
+        }
+      }
+    }
+
+    if (non_remainder && non_rows == 2) {
+      if (last_non[1] < 1) {
+        if (x < 4) {
+          return fail;
+        }
+        b[2][1] = '.';
+        b[2][2] = '.';
+        b[0][x-1] = '*';
+        b[1][x-1] = '*';
+      }
+    }
+
+    /*
+    if (x > 1 && last_non[0] > 0 && last_non[1] === 0) {
+      b[last_non[0]][last_non[1] + 1] = '.';
+      b[last_non[0]-1][x-1] = '*';
+    }
+    */
+
+    /*
+    if (non_rows == 1 && non_remainder) {
+      console.warn("new problem");
+
+      if ((x - non_remainder) % 2) {
+        //return fail;
+      }
+    }
+    */
+
+    b[0][0] = 'c';
+
+    var nx = x;
+    var ny = y;
+
+    return str(b);
+  }
+
+  res(cnum, solve());
 })
